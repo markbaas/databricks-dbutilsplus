db54724e301c3704cbd78a6dc9a36135d88d978b
Mark Baas <mbaas@alfa.nl>
2025-04-16 15:30:33 +0200

    support for abfss

--- dbutilsplus/dbutils.py
+++ dbutilsplus/dbutils.py
@@ -1,49 +1,49 @@
 import json
 import logging
+from pathlib import Path
+
 from azure.identity import DefaultAzureCredential
+from azure.storage.filedatalake import DataLakeServiceClient
 from databricks.sdk import WorkspaceClient
 from databricks.sdk._widgets.ipywidgets_utils import DbUtilsWidget
+from databricks.sdk.dbutils import FileInfo
 from ipywidgets.widgets import widget_string
+
+w = WorkspaceClient()
 
 
 class Widgets:
-    def __init__(self, _widgets):
-        self._widgets = _widgets
-
     def _register(self, name, widget, label=None):
         label = label if label is not None else name
-        w = DbUtilsWidget(label, widget)
+        widget = DbUtilsWidget(label, widget)
 
-        if name in self._widgets._widgets:
+        if name in w.dbutils.widgets._widgets:
             self.remove(name)
 
-        self._widgets._widgets[name] = w
+        w.dbutils.widgets._widgets[name] = widget
 
     def _refreshParameters(self):
-        with open('.params.json', 'r') as f:
+        with open(".params.json", "r") as f:
             for k, v in json.load(f).items():
                 self._register(k, widget_string.Text(v))
 
     def getAll(self):
         self._refreshParameters()
-        return {x: y.value for x, y in self._widgets._widgets.items()}
+        return {x: y.value for x, y in w.dbutils.widgets._widgets.items()}
 
     def getArgument(self, *args, **kwargs):
         self._refreshParameters()
-        self._widgets.getArgument(*args, **kwargs)
+        w.dbutils.widgets.getArgument(*args, **kwargs)
 
     def get(self, *args, **kwargs):
         self._refreshParameters()
-        self._widgets.get(*args, **kwargs)
+        w.dbutils.widgets.get(*args, **kwargs)
 
     def __getattr__(self, name):
-        return getattr(self._widgets, name)
+        return getattr(w.dbutils.widgets, name)
 
 
 class Credentials:
-    def __init__(self, credentials):
-        self._credentials = credentials
-
     def getServiceCredentialsProvider(self, *args, **kwargs):
         logging.warning(
             "getServiceCredentialsProvider is not supported remotely. Using DefaultAzureCredential instead."
@@ -51,17 +51,56 @@ class Credentials:
         return DefaultAzureCredential()
 
     def __getattr__(self, name):
-        return getattr(self._credentials, name)
+        return getattr(w.dbutils.credentials, name)
+
+
+class Fs:
+    def _convert_abfss_to_adls_uri(self, abfss_uri):
+        parts = abfss_uri.split("@")
+        container = parts[0].replace("abfss://", "")
+        account_name = parts[1].split(".")[0]
+        path = "/".join(parts[1].split("/")[1:])
+        return f"https://{account_name}.dfs.core.windows.net/", container, path
+
+    def ls_adls(self, input_path):
+        # Authenticate and list files
+        adls_uri, container, path = self._convert_abfss_to_adls_uri(input_path)
+        service_client = DataLakeServiceClient(
+            account_url=adls_uri, credential=DefaultAzureCredential()
+        )
+        file_system_client = service_client.get_file_system_client(file_system=container)
+
+        # List files
+        paths = file_system_client.get_paths(path=path, recursive=False)
+        for path in paths:
+            yield FileInfo(
+                path.name,
+                Path(path.name).name,
+                path.content_length,
+                int(path.last_modified.timestamp() * 1000),
+            )
+
+    def ls(self, path):
+        try:
+            return w.dbutils.fs.ls(path)
+        except Exception as e:
+            if 'unsupported scheme "abfss"' in str(e):
+                return self.ls_adls(path)
+            else:
+                raise e
+
+    def __getattr__(self, name):
+        return getattr(w.dbutils.fs, name)
 
 
 class DbUtils:
     def __init__(self):
-        self.w = WorkspaceClient()
-        self.widgets = Widgets(self.w.dbutils.widgets)
-        self.credentials = Credentials(self.w.dbutils.credentials)
+        self.widgets = Widgets()
+        self.credentials = Credentials()
+        self.fs = Fs()
 
     def __getattr__(self, name):
-        return getattr(self.w.dbutils, name)
+        return getattr(w.dbutils, name)
 
 
 dbutils = DbUtils()
